f581bb39234e801ecd4b31b009cc5d786d102ddd
kritivaish <vkriti107@gmail.com>
2020-10-08 00:06:57 -0500

    initial commit

---
+++ public/scripts/app.js
@@ -0,0 +1,10 @@
+// IIFE --Immediately Invoked Function Expressions
+(function(){
+
+    function Start()
+    {
+        console.log("App Started...");
+    }
+
+    window.addEventListener("load", Start);
+});
